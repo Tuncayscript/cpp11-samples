0426478c19e87c8d840f1f3a4323e124c8dd9ddc
Tuncay Akberli <128295620+Tuncayscript@users.noreply.github.com>
2024-02-18 14:58:35 +0400

    Add files via upload

---
+++ 02_ref-qualifiers_sample.cpp
@@ -0,0 +1,27 @@
+// cpp02.cpp : Defines the entry point for the console application.
+//
+// Compiler: Visual C++ 2015 Preview
+
+#include "stdafx.h"
+#include <iostream>
+
+struct X {
+	void f()& {
+		std::cout << "L value" << std::endl;
+	}
+	void f() && {
+		std::cout << "R value" << std::endl;
+	}
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	X m;
+
+	std::wcout << "----- ref-qualifiers sample" << std::endl;
+	m.f();			// L value
+	std::move(m).f();	// R value
+
+	return 0;
+}
+
